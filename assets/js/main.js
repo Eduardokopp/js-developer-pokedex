4958c94ba87049a2d4b32f8b7744265653770b91
Eduardo Kopp Santos <eduardokopp3366@gmail.com>
2023-04-22 13:52:29 -0300

    Otimizando CSS para Bootstrap
    Foi realizado a alteração de todo o css para a aplicação do framework Bootstrap 5.0 via link

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,19 +7,25 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
+    <div id="pokemon"class="col-12 col-sm-6 col-md-4 col-lg-3 text-white h-100">
+        <div class="pokemon ${pokemon.type}">
+            <div class="w-100 text-end">
+                <span class="number">#${pokemon.number}</span>
             </div>
-        </li>
+            <div class="w-100 text-start mb-2 ">
+                <span class="fw-light text-capitalize">${pokemon.name}</span>
+            </div>
+            <div class="row">
+                <div class="col d-flex flex-column align-self-center">
+                    ${pokemon.types.map((type) =>
+                    `<span class="type fw-light w-75 text-capitalize ${type}">${type}</span>`).join('')}
+                </div>
+                <div class="col text-end">
+                    <img class="img-fluid" style="max-height: 70px;" src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+            </div>
+        </div>
+    </div>
     `
 }
 
